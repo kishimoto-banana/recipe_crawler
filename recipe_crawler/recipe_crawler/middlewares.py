77bf5e194e496d06223337361c7978c00c568e60
kishimoto <drehbleistift@gmail.com>
2021-07-10 22:34:02 +0900

    Fmt

--- recipe_crawler/recipe_crawler/middlewares.py
+++ recipe_crawler/recipe_crawler/middlewares.py
@@ -53,7 +53,7 @@ class RecipeCrawlerSpiderMiddleware:
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class RecipeCrawlerDownloaderMiddleware:
@@ -100,4 +100,4 @@ class RecipeCrawlerDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
